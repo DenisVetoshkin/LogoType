17be77df737d2f3fd1448d307bfb6370e71cee5f
Denis Desctop Vetoshkin <lotus-brest@yandex.ru>
2020-10-07 23:09:24 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('.slider__inner').slick({
+    nextArrow: '<button type="button" class="slick-next slick__btn"></button>',
+    prevArrow: '<button type="button" class="slick-prev slick__btn"></button>',
+  });
+
+
+
+
+
+
+
+});

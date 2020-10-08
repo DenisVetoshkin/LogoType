1046bf7e3432d5f3d36f463f786af88cd759ebc7
Denis Desctop Vetoshkin <lotus-brest@yandex.ru>
2020-10-08 03:02:48 +0300

    07.10.2020

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(function() {
   $('.slider__inner').slick({
     nextArrow: '<button type="button" class="slick-next slick__btn"></button>',
     prevArrow: '<button type="button" class="slick-prev slick__btn"></button>',
+    infinite: false,
   });
 
 
